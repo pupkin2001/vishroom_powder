c692fcf1f9c67f35017f97fc0a2f63a31047fc34
pupkin2001 <131904062+pupkin2001@users.noreply.github.com>
2023-12-11 17:58:00 +0500

    Update v0.8.1
    - Made sweet sugar cube recipe less brutal
    - Increase speed buff from Clairvoyance
    - Add mining speed buff from Clairvoyance
    - Tweak duration of effects from sweet sugar and sweet sugar cube
    - Update sweet sugar and sweet sugar cube textures to better fit vanilla syle
    
    Know issues:
    - Clairvoyance doesn't modify player attributes

--- src/main/java/pupkin/mod/init/ItemInit.java
+++ src/main/java/pupkin/mod/init/ItemInit.java
@@ -3,8 +3,8 @@ package pupkin.mod.init;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.potion.PotionEffect;
+import pupkin.mod.item.ItemEdibleWithEffects;
 import pupkin.mod.potion.ClairvoyanceEffect;
-import pupkin.mod.potion.ItemVishroomPowder;
 import pupkin.mod.potion.YellowTintEffect;
 
 import java.util.ArrayList;
@@ -15,21 +15,21 @@ public class ItemInit
 	public static final List<Item> ITEMS = new ArrayList<>();
 
 	private static final List<PotionEffect> vishroomPowderEffects = new ArrayList<>();
-	public static final Item VISHROOM_POWDER = new ItemVishroomPowder("vishroom_powder", CreativeTabs.FOOD, 1, 1, false, vishroomPowderEffects);
+	public static final Item VISHROOM_POWDER = new ItemEdibleWithEffects("vishroom_powder", CreativeTabs.FOOD, 1, 1, false, vishroomPowderEffects);
 	private static final List<PotionEffect> sweetSugarEffects = new ArrayList<>();
-	public static final Item SWEET_SUGAR = new ItemVishroomPowder("sweet_sugar", CreativeTabs.FOOD, 0, 2, false, sweetSugarEffects);
+	public static final Item SWEET_SUGAR = new ItemEdibleWithEffects("sweet_sugar", CreativeTabs.FOOD, 0, 2, false, sweetSugarEffects);
 	private static final List<PotionEffect> sweetSugarCubeEffects = new ArrayList<>();
-	public static final Item SWEET_SUGAR_CUBE = new ItemVishroomPowder("sweet_sugar_cube", CreativeTabs.FOOD, 0, 4, false, sweetSugarCubeEffects);
+	public static final Item SWEET_SUGAR_CUBE = new ItemEdibleWithEffects("sweet_sugar_cube", CreativeTabs.FOOD, 0, 4, false, sweetSugarCubeEffects);
 
 	static {
 		vishroomPowderEffects.add(new PotionEffect(YellowTintEffect.YELLOW_TINT, 1200, 0));
 	}
 
 	static {
-		sweetSugarEffects.add(new PotionEffect(ClairvoyanceEffect.CLAIRVOYANCE, 600, 0));
+		sweetSugarEffects.add(new PotionEffect(ClairvoyanceEffect.CLAIRVOYANCE, 540, 0));
 	}
 
 	static {
-		sweetSugarCubeEffects.add(new PotionEffect(ClairvoyanceEffect.CLAIRVOYANCE, 1800, 1));
+		sweetSugarCubeEffects.add(new PotionEffect(ClairvoyanceEffect.CLAIRVOYANCE, 1620, 1));
 	}
 }
